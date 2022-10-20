3bb50429fd7f913d4aaec96e96034cc0d3585db3
Mattijs Bliek <mattijs@mollie.com>
2022-10-20 13:23:28 +0200

    Eindopdracht

--- dag3/01-tetris/script.js
+++ dag3/01-tetris/script.js
@@ -11,3 +11,56 @@ Bonus:
 - Zet het vierkant elke keer dat je op enter drukt terug in de linker bovenhoek
 - Maak van het vierkant een rechthoek, en draai het 90 graden elke keer dat je op spatie drukt
 */
+let vierkant = document.querySelector('.box');
+vierkant.style.top = '0px';
+vierkant.style.left = '0px';
+
+let graden = 0;
+
+let kleuren = [
+  'rgb(62, 189, 208)',
+  'rgb(30, 30, 122)',
+  'rgb(233, 70, 100)',
+  'rgb(150, 232, 0)',
+];
+
+document.addEventListener('keydown', drukOpToets);
+
+function drukOpToets(event) {
+  if (event.key === 'ArrowRight') {
+    vierkant.style.left = parseInt(vierkant.style.left) + 100 + "px";
+  }
+
+  if (event.key === 'ArrowLeft') {
+    vierkant.style.left = parseInt(vierkant.style.left) - 100 + "px";
+  }
+
+  if (event.key === 'ArrowUp') {
+    vierkant.style.top = parseInt(vierkant.style.top) - 100 + "px";
+  }
+
+  if (event.key === 'ArrowDown') {
+    vierkant.style.top = parseInt(vierkant.style.top) + 100 + "px";
+  }
+
+  if (event.key === ' ') {
+    graden += 90;
+    vierkant.style.transform = 'rotate(' + graden + 'deg)';
+  }
+
+  if (event.key === 'a') {
+    vierkant.style.background = kiesEenKleur();
+  }
+}
+
+let nummer = 0;
+
+function kiesEenKleur() {
+  nummer += 1;
+
+  if (nummer === kleuren.length) {
+    nummer = 0;
+  }
+
+  return kleuren[nummer];
+}
--- eindopdracht/README.md
+++ eindopdracht/README.md
@@ -1,3 +1,18 @@
 # Eindopdracht
 
-We gaan een website maken waarmee je foto's kan nemen en je foto's ook kan bekijken. 
+Je gaat je eigen website maken met vier pagina's:
+* Homepage: je naam en links naar de andere pagina's. Maak hem mooi met CSS.
+* Liedjes: je 5 favoriete nummers met Spotify embeds. Boven elke embed zet je de naam van de artiest,
+  de naam van het liedje, en een afbeelding van de artiest. 
+* Foto's: maak 
+
+## Spotify embeds
+1. Google op "naam liedje spotify"
+2. Klik op het Spotify zoekresultaat
+3. Klik op de drie puntjes
+4. Klik op "Share"
+5. Klik op "Embed"
+6. Klik op "Copy"
+7. Plak de code in je HTML.
+
+Je kan de hoogte en breedte van de embed aanpassen met CSS.
